e1cebf6dc92488e121f84dcd0264dab6564c1243
rafaeldamasceno <rafaeldamasceno@me.com>
2017-04-03 14:50:42 +0100

    Add remote target.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   BOARD_URL: 'https://www.reddit.com/api/place/board-bitmap',
   BOARD_FILE: __dirname + '/tmp/board.bmp',
 
-  REMOTE_TARGET_URL: 'https://raw.githubusercontent.com/Zequez/reddit-placebot/master/images/target.png',
+  REMOTE_TARGET_URL: 'https://raw.githubusercontent.com/rafaeldamasceno/reddit-placebot/master/target.png',
   REMOTE_TARGET_FILE: __dirname + '/tmp/remote_target',
 
   LOCAL_TARGET_FILE: __dirname + '/images/target.png',
@@ -11,7 +11,7 @@ module.exports = {
 
   // Use the REMOTE_TARGET_URL file as target, otherwise it's gonna just
   // try to read from target.bmp
-  useRemoteTarget: false,
+  useRemoteTarget: true,
 
   // Wait until these amount of accounts are available
   // and paint pixels at the same time
